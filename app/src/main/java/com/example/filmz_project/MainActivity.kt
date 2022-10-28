91172b21e27bb004ea267fd3c09e813548f06fd2
Joel <jmarcosc2122@politecnics.barcelona>
2022-10-28 13:52:55 +0200

    ha

--- app/src/main/java/com/example/filmz_project/MainActivity.kt
+++ app/src/main/java/com/example/filmz_project/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lenguage_screen)
+        setContentView(R.layout.question_screen)
     }
 }
