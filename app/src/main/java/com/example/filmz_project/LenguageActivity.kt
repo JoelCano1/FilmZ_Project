cb0fd8c85de2110f520fd3f9d8b9f3859f39e0c4
Joel <jmarcosc2122@politecnics.barcelona>
2022-11-08 13:20:22 +0100

    g

--- app/src/main/java/com/example/filmz_project/LenguageActivity.kt
+++ app/src/main/java/com/example/filmz_project/LenguageActivity.kt
@@ -2,10 +2,22 @@ package com.example.filmz_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class LenguageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.lenguage_screen)
+
+
+        val spanishFlag = findViewById(R.id.BtnCastellano) as ImageView
+
+        spanishFlag.animate().apply {
+
+            duration = 1500
+            alpha(1f)
+            rotationYBy(180f)
+        }.start()
     }
 }
