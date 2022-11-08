41880d96532ba809fae4a01e8330296c636129b5
MarioMolina27 <molinaballesteros@hotmail.com>
2022-11-08 12:48:24 +0100

    ranking animation

--- app/src/main/java/com/example/filmz_project/RankingActivity.kt
+++ app/src/main/java/com/example/filmz_project/RankingActivity.kt
@@ -2,10 +2,22 @@ package com.example.filmz_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.LinearLayout
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ranking_screen)
+
+        val linearRanking = findViewById<LinearLayout>(R.id.linearRanking)
+        val btnContinuarRanking = findViewById<Button>(R.id.btnContinuarRanking)
+        val animation : Animation = AnimationUtils.loadAnimation(this,R.anim.move_down)
+        linearRanking.startAnimation(animation)
+        btnContinuarRanking.startAnimation(animation)
+
     }
+
 }
