5b76dbdb7abd2339fe6f174f7ac66ce331184af2
AlbertPolitecnis <99322526+AlbertPolitecnis@users.noreply.github.com>
2022-11-09 13:16:45 +0100

    sortir + strings

--- app/src/main/java/com/example/filmz_project/FinalActivity.kt
+++ app/src/main/java/com/example/filmz_project/FinalActivity.kt
@@ -1,11 +1,32 @@
 package com.example.filmz_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FinalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.final_screen)
+
+        val btnTornarJugar = findViewById(R.id.BtnTornarJugar) as Button
+        val btnSortirApp = findViewById(R.id.BtnSortirApp) as Button
+
+        btnTornarJugar.setOnClickListener() {
+
+            val user = "a"
+
+            val intent =  Intent(this, DifficultActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
+        btnSortirApp.setOnClickListener() {
+
+            finishAffinity();
+
+        }
     }
 }
