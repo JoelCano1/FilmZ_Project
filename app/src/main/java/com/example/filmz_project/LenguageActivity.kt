5e6c0e6c6ca2deff5b9f31b208bd54e4ed3438d0
Joel <jmarcosc2122@politecnics.barcelona>
2022-11-09 12:48:46 +0100

    last

--- app/src/main/java/com/example/filmz_project/LenguageActivity.kt
+++ app/src/main/java/com/example/filmz_project/LenguageActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 
 class LenguageActivity : AppCompatActivity() {
@@ -14,17 +14,20 @@ class LenguageActivity : AppCompatActivity() {
         setContentView(R.layout.lenguage_screen)
 
         //animacion
-        val animation1: Animation = AnimationUtils.loadAnimation(this, R.anim.flip_flag)
-        val animation2: Animation = AnimationUtils.loadAnimation(this, R.anim.flip_flag)
+        val animation1: Animation = AnimationUtils.loadAnimation(this, R.anim.spawn)
+        val animation2: Animation = AnimationUtils.loadAnimation(this, R.anim.move_up)
+        val animation3: Animation = AnimationUtils.loadAnimation(this, R.anim.move_down2)
         val spanishFlag = findViewById(R.id.BtnCastellano) as ImageView
         val catalanFlag = findViewById(R.id.BtnCatala) as ImageView
         val englishFlag = findViewById(R.id.BtnEnglish) as ImageView
         val lblLenguaje = findViewById(R.id.lblLenguaje) as TextView
+        val btnNext = findViewById(R.id.btonNextLeng) as ImageView
+
         spanishFlag.startAnimation(animation1)
         catalanFlag.startAnimation(animation1)
         englishFlag.startAnimation(animation1)
         lblLenguaje.startAnimation(animation2)
-
+        btnNext.startAnimation(animation3)
 
     }
 }
