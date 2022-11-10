77a2e0937c22b1893a2e3d4eb7e0c00b0338f932
Pol Crespo <pcrespoh2122@politecnics.barcelona>
2022-11-10 12:13:00 +0100

    canvis

--- app/src/main/java/com/example/filmz_project/ResultActivity.kt
+++ app/src/main/java/com/example/filmz_project/ResultActivity.kt
@@ -1,11 +1,32 @@
 package com.example.filmz_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.result_screen)
+
+        veureRanking()
+        continuar()
+    }
+    private fun veureRanking() {
+        val btnRanking = findViewById<Button>(R.id.BtnRankingResult)
+
+        btnRanking.setOnClickListener() {
+            val intent = Intent(this, RankingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun continuar() {
+        val btnContinuar = findViewById<Button>(R.id.BtnContinuarResult)
+
+        btnContinuar.setOnClickListener() {
+            val intent = Intent(this, FinalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
