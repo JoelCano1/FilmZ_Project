9718dab5fe1b6b72a706f821e4f6f577e15cacf3
Joel <jmarcosc2122@politecnics.barcelona>
2022-11-10 13:58:32 +0100

    edit

--- app/src/main/java/com/example/filmz_project/QuestionActivity.kt
+++ app/src/main/java/com/example/filmz_project/QuestionActivity.kt
@@ -2,10 +2,34 @@ package com.example.filmz_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuestionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.question_screen)
+
+        val timeBar = findViewById(R.id.ProgressBar) as ProgressBar
+        val pros = findViewById<TextView>(R.id.NumPreg)
+        timeBar.max = 75
+        timeBar.progress = 0
+
+        object : CountDownTimer(15_000, 1){
+            override fun onTick(remaining: Long) {
+                timeBar.progress = (timeBar.progress+1)
+                //pros.text = remaining.toString()
+            }
+
+            override fun onFinish() {
+
+            }
+        }.start()
+
+
+
     }
 }
