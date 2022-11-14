ad67604ffc2902574309ef6ae75c972506322d03
AlbertPolitecnis <99322526+AlbertPolitecnis@users.noreply.github.com>
2022-11-14 13:50:10 +0100

    Keys

--- app/src/main/java/com/example/filmz_project/LoginActivity.kt
+++ app/src/main/java/com/example/filmz_project/LoginActivity.kt
@@ -72,6 +72,8 @@ class LoginActivity : AppCompatActivity() {
 
                 val intent = Intent(this, DifficultActivity::class.java)
 
+                intent.putExtra(Keys.constKeys.LOGIN_TO_DIFFICULTY,user)
+
                 startActivity(intent)
 
             } else {
