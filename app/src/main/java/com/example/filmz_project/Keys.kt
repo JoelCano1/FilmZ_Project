c4d07c5cac1cbd3d642629293c8a28b7c9360df1
AlbertPolitecnis <99322526+AlbertPolitecnis@users.noreply.github.com>
2022-11-21 13:27:56 +0100

    changes

--- app/src/main/java/com/example/filmz_project/Keys.kt
+++ app/src/main/java/com/example/filmz_project/Keys.kt
@@ -4,11 +4,10 @@ class Keys {
 
     object constKeys {
 
-        const val LOGIN_TO_DIFFICULTY = "USER"
+        const val TO_DIFFICULTY = "USER"
         const val REGISTER_TO_LOGIN = "NEWUSER"
         const val DIFFICULT_TO_QUIZ = "PLAY"
         const val RANKING_TO_FINAL = "FINAL"
-        const val FINAL_TO_DIFFICULTY = "REPLAY"
 
     }
 }
