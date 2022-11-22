8b4ab0300bdc8281c6b55121f969236138dfc75d
MarioMolina27 <molinaballesteros@hotmail.com>
2022-11-22 13:51:18 +0100

    segundo cambio register

--- app/src/main/java/com/example/filmz_project/Keys.kt
+++ app/src/main/java/com/example/filmz_project/Keys.kt
@@ -3,12 +3,9 @@ package com.example.filmz_project
 class Keys {
 
     object constKeys {
-
-        const val LOGIN_TO_DIFFICULTY = "USER"
-        const val REGISTER_TO_LOGIN = "NEWUSER"
         const val DIFFICULT_TO_QUIZ = "PLAY"
         const val RANKING_TO_FINAL = "FINAL"
-        const val FINAL_TO_DIFFICULTY = "REPLAY"
+        const val TO_DIFFICULTY = "USER"
         const val QUESTIONS_TO_RESULT = "USER"
         const val QUESTIONS_TO_RESULT2 = "RESULTS"
 
