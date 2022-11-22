d98b0c90a1d223cb8816e9f6f2077492b9f972b0
JoelCano1 <jmarcosc2122@politecnics.barcelona>
2022-11-22 20:19:58 +0100

    s

--- app/src/main/java/com/example/filmz_project/QuestionActivity.kt
+++ app/src/main/java/com/example/filmz_project/QuestionActivity.kt
@@ -301,7 +301,7 @@ class QuestionActivity : AppCompatActivity() {
 
         val intent = getIntent()
         //var jugadorActual = intent.getSerializableExtra(Keys.constKeys.DIFFICULT_TO_QUIZ) as User
-        val jugadorActual = User("Juan", "123", 18,true, 'H', 148, true, 3, null)
+        val jugadorActual = User("Juan", "123", 18,true, 'H', 148, true, 2, null)
 
         //cargamos el json una vez
         val loadedJSON = loadQuestions(jugadorActual)
